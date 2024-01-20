f2102bb24a5f9d1b9e937ff9bff8f2a87a66d184
shresthaaman27@gmail.com <shresthaaman27@gmail.com>
2024-01-20 21:44:26 +0545

    login and register added and linked with database

--- E_shop/views.py
+++ E_shop/views.py
@@ -1,5 +1,7 @@
+from django.views import View
 from django.shortcuts import render, redirect
 from store_app.models import Product
+from django.contrib.auth.models import User
 
 def BASE(request):
     return render(request, 'main/base.html')
@@ -13,12 +15,34 @@ def HOME(request):
 
     return render(request, 'main/index.html', context)
 
-
 def product(request):
-
     products = Product.objects.all()
 
     context = {
         'products': products,
     }
+
     return render(request, 'main/product.html', context)
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        pass1 =  request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        customer = User.objects.create_user(username, email, pass1)
+        customer.first_name = first_name
+        customer.last_name = last_name
+        customer.save()
+
+        return redirect('home')
+
+    return render(request, 'register/auth.html')
+
+class AuthView(View):
+    def get(self, request):
+        # Your view logic here
+        return render(request, 'register/auth.html')
---
+++ static/js/scripts.js
@@ -0,0 +1,9 @@
+function showForm(formType) {
+  if (formType === 'login') {
+    document.querySelector('.login-form').style.display = 'block';
+    document.querySelector('.register-form').style.display = 'none';
+  } else if (formType === 'register') {
+    document.querySelector('.login-form').style.display = 'none';
+    document.querySelector('.register-form').style.display = 'block';
+  }
+}
