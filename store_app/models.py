c4cbe74dfabd8d3a779ee8ef2cc6de9a2141da0b
shresthaaman27@gmail.com <shresthaaman27@gmail.com>
2024-04-13 11:31:13 +0545

    review feature

--- store_app/models.py
+++ store_app/models.py
@@ -142,3 +142,12 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_method = models.CharField(max_length=100)
     payment_date = models.DateTimeField(auto_now_add=True)
+
+
+
+class ProductReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
